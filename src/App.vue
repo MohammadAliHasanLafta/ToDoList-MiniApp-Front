0d9923456ac86aaa30d24655a2316cb736e51cae
MohammadAliHasanLafta <1380lafta@gmail.com>
2024-11-06 17:00:41 +0330

    Navbar color fixed

--- src/App.vue
+++ src/App.vue
@@ -71,7 +71,7 @@
       :todos="todos"
       :mobile="mobile"/>
 
-      <nav class="fixed bottom-0 right-0 w-full bg-white shadow-lg flex justify-around items-center py-4 text-gray-600 border-t border-gray-200">
+      <nav class="fixed bottom-0 right-0 w-full shadow-lg flex justify-around items-center py-4 text-gray-600 border-t border-gray-200">
         <router-link to="/profile" class="flex flex-col items-center text-center text-sm" active-class="text-[#FF8100]">
           <i class="fa-solid fa-user text-2xl"></i>
         </router-link>
@@ -590,7 +590,7 @@ export default {
 }
 
 nav {
-  background-color: var(--navbar-background); /* رنگ پس‌زمینه نوار ناوبری */
+  background-color: var(--navbar-background); 
   color: var(--navbar-text); /* رنگ متن نوار ناوبری */
   border-top: 1px solid var(--text-color); /* رنگ مرزی */
 }
